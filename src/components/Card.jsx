00334f15c070cdbd3b75e209f0448938c20e570b
lee <mikan31@naver.com>
2025-05-21 15:22:18 +0900

    5월 21일 과제 2번

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,5 @@
 export default function Card({ item }) {
+    const {title, description, categodry} = item;
     //1. item을 받아와서 카드를 렌더링 해야 합니다.
     //2. item의 title, description, category를 각각 h2, p, span태그에 알맞게 넣으세요
     //3. item은 CardList에서 전달됩니다.
@@ -7,7 +8,7 @@ export default function Card({ item }) {
     return (
         <div className="card">
             <div className="card-image">
-                <img src={`https://placehold.co/600x400`} />
+                <img src={`https://placehold.co/600x400`} alt={title}/>alt={title}
             </div>
             <div className="card-content">
                 <span className="card-category">???</span>
